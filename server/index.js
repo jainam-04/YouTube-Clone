77d678be64820f0ff9aa2af14076a2de0571bb9e
Jainam Rupani <jainamrupani04@gmail.com>
2025-06-11 16:47:23 +0530

    Backend setup is done

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from "body-parser"
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.get('/', (req, res) => {
+      res.send("YouTube Clone is working!")
+})
+app.use(bodyParser.json())
+const port = process.env.port || 5000
+app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`)
+})
+const DB_URL = process.env.DB_URL
+mongoose.connect(DB_URL).then(() => {
+      console.log("MongoDB Database Connected!")
+}).catch((error) => {
+      console.log(error)
+})
